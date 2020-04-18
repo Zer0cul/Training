29381d4ac60096cdf74ec08be09350cb37a4d6cc
vitaliiboiko360 <vitaliiboiko360@gmail.com>
2020-04-18 13:59:46 +0300

    Update main.c
    applied linux convention in function signatures
    and fixed GetRevenue calls

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "vmfunction.h"
-int main(int argc, char** argv)
+
+int 
+main(int argc, char** argv)
 {
-    GetRevenue();
+    if (argc < 2)
+    {
+        printf("aguments are");
+        for (int i=0; i<argc; ++i)
+        {
+            printf("arg #%i is \"%s\"\n", i, atoi(argc[i]);
+        }
+        return 0;
+    }
+    
+    return GetRevenue(atoi(argv[1]), atoi(argv[2]));
 }
